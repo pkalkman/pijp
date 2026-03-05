3cbdb53e7011c054650388d26d21833804736ac3
Peter Kalkman <peter.kalkman@partners4it.nl>
2026-03-05 18:38:48 +0100

    neon postgress

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 const { pouleOna, poulePijp, swapInPouleOna, swapInPoulePijp } = usePijpStore();
+
+const { data, error } = await useFetch('/api/version');
 </script>
 
 <template>
@@ -7,5 +9,10 @@ const { pouleOna, poulePijp, swapInPouleOna, swapInPoulePijp } = usePijpStore();
     <PouleKaart v-if="pouleOna" :poule="pouleOna" titel="Ona" @swap="swapInPouleOna" />
     <PouleKaart v-if="poulePijp" :poule="poulePijp" titel="Pijp" @swap="swapInPoulePijp" />
     <PijpSettingsForm />
+    <div>
+      <h1>Database Version</h1>
+      <p v-if="data">{{ data.version }}</p>
+      <p v-else-if="error">Error fetching version</p>
+    </div>
   </div>
 </template>
