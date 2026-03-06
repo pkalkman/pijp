176d26d2adfcabf6ab26ed166d178fa27b06040c
Peter Kalkman <peter.kalkman@partners4it.nl>
2026-03-06 19:25:05 +0100

    geen create user meer

--- server/plugins/01.start.ts
+++ server/plugins/01.start.ts
@@ -1,16 +1,6 @@
-import { auth } from '~~/lib/auth';
-
 export default defineNitroPlugin(async () => {
   try {
     console.log('... connected database name', db.databaseName);
-
-    await auth.api.createUser({
-      body: {
-        email: 'peter.kalkman@pkit2.nl',
-        password: 'Pijp2026!!',
-        name: 'Peter Kalkman',
-      },
-    });
   } catch (error) {
     console.error('Nitro startup error', error);
   }
