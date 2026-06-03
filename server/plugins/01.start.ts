a866eb3bf4cf94c42c6d29e944f2fa15b6e1bf90
Peter Kalkman <peter.kalkman@partners4it.nl>
2026-06-03 16:14:01 +0200

    weg password

--- server/plugins/01.start.ts
+++ server/plugins/01.start.ts
@@ -1,29 +1,6 @@
-import { auth } from '~~/lib/auth';
-
 export default defineNitroPlugin(async () => {
   try {
     console.log('... connected database name', db.databaseName);
-
-    const newUser = await auth.api.createUser({
-      body: {
-        email: 'peter.kalkman@pkit.nl', // required
-        password: '3Banden!!', // required
-        name: 'Peter Kalkman', // required
-        role: 'user',
-      },
-    });
-
-    const newUser2 = await auth.api.createUser({
-      body: {
-        email: 'm.a.van.kooten@gmail.com', // required
-        password: '3Banden!!', // required
-        name: 'Marcel van Kooten', // required
-        role: 'user',
-      },
-    });
-
-    //
-    // console.log(newUser);
   } catch (error) {
     console.error('Nitro startup error', error);
   }
